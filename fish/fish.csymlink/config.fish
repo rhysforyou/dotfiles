2e6293fdf1072bcdad413fec9537115f2c2ee9a2
Rhys Powell <rhys@rpowell.me>
2023-01-09 12:57:59 +1100

    remove Rust, Android, and Flutter from fish config

--- fish/fish.csymlink/config.fish
+++ fish/fish.csymlink/config.fish
@@ -1,26 +1,15 @@
+# Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Node
 status --is-interactive; and source (nodenv init -|psub)
 
 # Ruby
 status --is-interactive; and source (rbenv init -|psub)
 
-# Use NeoVim as my text edito
+# Use NeoVim as my text editor
 set EDITOR '/usr/local/bin/nvim'
 set VISUAL '/usr/local/bin/nvim'
-
-# Rust
-set -x PATH $PATH "$HOME/.cargo/bin"
-
-# Android
-set JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home"
-set -x ANDROID_HOME $HOME/Library/Android/sdk
-set -x PATH $PATH $ANDROID_HOME/emulator
-set -x PATH $PATH $ANDROID_HOME/tools
-set -x PATH $PATH $ANDROID_HOME/tools/bin
-set -x PATH $PATH $ANDROID_HOME/platform-tools
-
-# Flutter
-set -x PATH $PATH "$HOME/Library/flutter/bin"
 
 # Dotfiles
 set -x PATH $PATH "$HOME/.dotfiles/bin"
